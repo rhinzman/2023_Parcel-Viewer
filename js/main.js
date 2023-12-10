50125859fbd1bd2b69c87b4e85bc4deac8e39b96
lsamo02 <samotis@wisc.edu>
2023-12-09 21:11:50 -0500

    main js update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 //step 1 create map
 var attributes; 
+var selection;
 
 function createMap(){
   
@@ -21,6 +22,22 @@ function createMap(){
 	ext: 'png'
 }).addTo(map);
 var baseMaps = {Stadia_AlidadeSmoothDark, basemap};
+
+// define function to handle click events on parcels
+function parcelOnEachFeature(feature, layer){
+  layer.on({
+    click: function(e) {
+      if (selection) {
+        parcelLayerLayer.resetStyle(selection);
+      }
+              
+      e.target.setStyle(parcelSelectedStyle());
+      selection = e.target;
+
+      L.DomEvent.stopPropagation(e); // stop click event from being propagated down to other elements
+    }
+  });
+}
     
 
       var township = L.esri
@@ -33,7 +50,7 @@ var baseMaps = {Stadia_AlidadeSmoothDark, basemap};
     
       var county = L.esri
         .featureLayer({
-          url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/Parcel_Viewer_WFL1/FeatureServer/1"
+          url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/Parcel_Viewer_WFL1/FeatureServer/1", style:countyStyle
         });
 
       county.addTo(map);
@@ -46,7 +63,7 @@ var baseMaps = {Stadia_AlidadeSmoothDark, basemap};
 
     var parcelLayer = L.esri
         .featureLayer({
-          url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/Parcel_Viewer_WFL1/FeatureServer/3", style:parcelStyle
+          url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/Parcel_Viewer_WFL1/FeatureServer/3", style:parcelStyle, onEachFeature: parcelOnEachFeature
         }).addTo(map);
         parcelLayer.bindPopup(function(layer){
           return L.Util.template('<p><b>Parcel ID: </b> {Name}</p>'+'<p><b>Township: </b> {Township}</p>'+'<p><b>Zip Code: </b> {Zip_Code}</p>', layer.feature.properties);
@@ -79,10 +96,10 @@ var baseMaps = {Stadia_AlidadeSmoothDark, basemap};
   legend.onAdd = function(map) {
     var div = L.DomUtil.create("div", "legend");
     div.innerHTML += "<h4>Legend</h4>";
-    div.innerHTML += '<i style="background: #477AC2"></i><span>County</span><br>';
-    div.innerHTML += '<i style="background: #448D40"></i><span>Township</span><br>';
-    div.innerHTML += '<i style="background: #FA8072"></i><span>Parcel</span><br>';
-    div.innerHTML += '<i style="background: #E8E6E0"></i><span>Zipcode</span><br>';
+    div.innerHTML += '<i style="background: #ffffff"></i><span>County</span><br>';
+    div.innerHTML += '<i style="background: #4C0073"></i><span>Township</span><br>';
+    div.innerHTML += '<i style="background: #000000"></i><span>Parcel</span><br>';
+    // div.innerHTML += '<i style="background: #E8E6E0"></i><span>Zipcode</span><br>';
     
     return div;
   };
@@ -96,23 +113,44 @@ function createParcelPopup(properties){
   return popUp; 
 }; 
 
-// Set up style for garden polygons
+// Set up style for parcel polys
 function parcelStyle(feature) {
   return {
-    fillColor: "#FA8072",
-    fillOpacity: 2,
-    color: '#CD5C5C',
-    weight: 2,
+    color: '#000000',
+    weight: 1.5,
   };
 }
 
 function townshipStyle(feature){
   return{
-    color: "#4C0073"
+    color: "#4C0073", 
+    weight: 2,
   }
 }
+function countyStyle(feature){
+  return{
+    color: "#ffffff",
+    weight: 1,
+  
+  };
+}
+// Blue symbol for selected gardens
+function parcelSelectedStyle(feature) {
+  return {
+    fillColor: "#00FFFB",
+    fillOpacity: 1,
+    color: '#0000FF',
+    weight: 4
+  };
+}
 
+// define and register event handler for click events to unselect features when clicked anywhere else on the map
+map.addEventListener('click', function(e) {
+  if (selection) {
+     gardenLayer.resetStyle(selection);
 
-
+     selection = null;
+  }
+});
 
 document.addEventListener('DOMContentLoaded',createMap)
